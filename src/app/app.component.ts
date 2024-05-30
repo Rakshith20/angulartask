67031d157efc60e4e7a2c91bceee487de52a7eec
Rakshith20 <shettykishan1992@gmail.com>
2024-05-31 00:05:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task';
+  users:any=[];
+  constructor(private userService: UserService){}
+
+  onSave(data:any){
+    console.log(data);
+
+    this.userService.addUser(data).subscribe((data)=>{
+      console.log(data);
+      this.getUsers();
+    })
+  }
+ngOnInit(): void {
+  this.getUsers();
+}
+
+getUsers(){
+  this.userService.getAllUsers().subscribe((users)=>{
+    console.log(users)
+    this.users=users;
+  })
+}
+
+  
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllUsers(){
+    return this.httpClient.get('http://localhost:3000/users');
+  }
+
+  addUser(user:any){
+    return this.httpClient.post('http://localhost:3000/users',user)
+  }
+}
