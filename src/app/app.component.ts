4dc9f1a546322233979c50349069ad26f04eb4f3
Rakshith20 <shettykishan1992@gmail.com>
2024-06-01 13:25:15 +0530

    added update and delete user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'task';
-  users:any=[];
-  constructor(private userService: UserService){}
+  users: any = [];
+  @ViewChild('register') registerForm: any;
+  isEditing: boolean = false;
+  editUserId: string = '';
+  constructor(private userService: UserService) {}
 
-  onSave(data:any){
-    console.log(data);
-
-    this.userService.addUser(data).subscribe((data)=>{
-      console.log(data);
-      this.getUsers();
-    })
+  onSave(data: any) {
+    if (this.isEditing) {
+      data.id = this.editUserId;
+      this.userService.editUser(data).subscribe((data) => {
+        this.getUsers();
+      });
+      this.isEditing = false;
+      this.editUserId = '';
+    } else {
+      this.userService.addUser(data).subscribe((data) => {
+        this.getUsers();
+      });
+    }
+    this.registerForm.reset();
   }
-ngOnInit(): void {
-  this.getUsers();
-}
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
-getUsers(){
-  this.userService.getAllUsers().subscribe((users)=>{
-    console.log(users)
-    this.users=users;
-  })
-}
+  getUsers() {
+    this.userService.getAllUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
 
-  
+  onDelete(id: string) {
+    this.userService.deleteUser(id).subscribe((users) => {
+      this.getUsers();
+    });
+  }
+
+  onEdit(user: any) {
+    this.isEditing = true;
+    this.editUserId = user.id;
+    this.registerForm.setValue({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      dob: user.dob,
+    });
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,17 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  getAllUsers(){
+  getAllUsers() {
     return this.httpClient.get('http://localhost:3000/users');
   }
 
-  addUser(user:any){
-    return this.httpClient.post('http://localhost:3000/users',user)
+  addUser(user: any) {
+    return this.httpClient.post('http://localhost:3000/users', user);
+  }
+
+  deleteUser(userId: string) {
+    return this.httpClient.delete(`http://localhost:3000/users/${userId}`);
+  }
+
+  editUser(user: any) {
+    return this.httpClient.put(`http://localhost:3000/users/${user.id}`, user);
   }
 }
